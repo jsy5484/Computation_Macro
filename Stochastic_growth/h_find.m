daa2e177d76ba9032c7c9571778c2a2c1e08040b
Seungyoon Jeong <syjeong@umich.edu>
2021-03-26 16:03:55 -0700

    Subroutine for solving the intra-temporal equation

--- Stochastic_growth/h_find.m
+++ Stochastic_growth/h_find.m
@@ -1,6 +1,10 @@
 function h_star = h_find(k, kp, z, psi, theta, grate, delta)
+
+    % Intratemporal Equation
     int = @(hr) (psi/(1-theta))*((k^theta)*(exp(z)*hr)^(1-theta) - grate*kp ...
         + (1-delta)*k) + ((hr-1)*(k^theta)*(exp(z)^(1-theta))*(hr^(-theta)));
+        
+    % Use a nonlinear solver    
     h_star = fsolve(int, 0.2);
     if h_star >= 1
         h_star = 0.9999;
