6b5e0fc5cd9592fe499bc29e456c1bc40a5b5f4e
Seungyoon Jeong <syjeong@umich.edu>
2021-03-26 16:06:14 -0700

    Subroutine for finding a steady-state of economy

--- Stochastic_growth/SS.m
+++ Stochastic_growth/SS.m
@@ -3,7 +3,8 @@
 function [kss, hss] = SS(param)
     F = @(x) [ param(5)*(param(1)*(x(1)^(param(1)-1))*(x(2)^(1-param(1))) + 1-param(2))- (1+param(4))*(1+param(3));
               (-(param(6)/(1-x(2)))) + ((1-param(1))*(x(1)^param(1))*(x(2)^(-param(1)))) / ((x(1)^param(1))*(x(2)^(1-param(1))) + (1-param(2))*x(1) - (1+param(4))*(1+param(3))*x(1))];
-    x0 = [0.5; 0.5];
     
+    % Initial guess
+    x0 = [0.5; 0.5];
     [kss, hss] = fsolve(F,x0);
 end
