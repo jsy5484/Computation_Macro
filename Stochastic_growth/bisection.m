6095f3a68be25e5a614d008efae7fa7bd40d6334
Seungyoon Jeong <syjeong@umich.edu>
2021-03-26 16:08:39 -0700

    Subroutine for solving the intra-temporal equation

--- Stochastic_growth/bisection.m
+++ Stochastic_growth/bisection.m
@@ -1,11 +1,8 @@
+% One-dimensional function Root-finding with the bisection method
 function p = bisection(f,a,b)
-% provide the equation you want to solve with R.H.S = 0 form. 
-% Write the L.H.S by using inline function
-% Give initial guesses.
-% Solves it by method of bisection.
-% A very simple code. But may come handy
+
 if f(a)*f(b)>0 
-    disp('Wrong choice bro')
+    disp('Wrong choice ')
 else
     p = (a + b)/2;
     err = abs(f(p));
